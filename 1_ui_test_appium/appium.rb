6b292a56aa242bff434ef0ed9f6d7572d21a8df0
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-05-22 16:16:00 -0700

    rename

--- 1_ui_test_appium/appium.rb
+++ 1_ui_test_appium/appium.rb
@@ -3,19 +3,15 @@ require 'appium_lib_core'
 
 class JaSSTTohoku < Minitest::Test
   def setup
+    # iOS 15 のシミュレータ(iPhone 12 Pro)でテストを実行する
     @core = ::Appium::Core.for capabilities: {
       platformName: :ios,
       automationName: :xcuitest,
       platformVersion: '15.0',
       deviceName: 'iPhone 12 Pro',
-      browserName: :safari,
-      wdaLaunchTimeout: 90_000
+      browserName: :safari
     }
     @driver = nil
-  end
-
-  def teardown
-    @driver&.quit
   end
 
   # 実際はよりシナリオを読みやすくするなどの工夫を行うことが多い
@@ -27,29 +23,40 @@ class JaSSTTohoku < Minitest::Test
     @driver.wait_true { |d| @driver.available_contexts.size >= 2 }
     @driver.context = @driver.available_contexts[-1]
 
+    # JaSST TohokuのURLが表示されるまで待つ
     assert @driver.wait_true { |d| d.current_url == 'https://jasst.jp/symposium/jasst22tohoku.html' }
 
-    3.times { w3c_scroll @driver }
+    # 3 回下方向にクロール
+    3.times { scroll @driver }
 
+    # "PAGE TOP" のマークをタップ
     @driver.context = 'NATIVE_APP'
-
     @driver.wait { |d| d.find_element :accessibility_id, 'PAGE TOP' }.click
 
+    # 画面に "JaSST開催概要" の要素が表示されていることを確認する
     e = @driver.wait { |d| d.find_element :accessibility_id, 'JaSST開催概要' }
     assert @driver.wait_true { e.displayed? }
   end
-end
 
-def w3c_scroll(driver)
-  window = driver.window_rect
-  action_builder = driver.action
-  input = action_builder.pointer_inputs[0]
-  action_builder
-    .move_to_location(window.width / 2, window.height * 8 / 10)
-    .pointer_down(:left)
-    .pause(input, 0.2)
-    .move_to_location(window.width / 2, window.height / 10)
-    .pause(input, 0.2)
-    .release
-    .perform
+  def teardown
+    @driver&.quit
+  end
+
+  private
+
+  def scroll
+    return if @driver.nil?
+
+    window = @driver.window_rect
+    action_builder = @driver.action
+    input = action_builder.pointer_inputs[0]
+    action_builder
+      .move_to_location(window.width / 2, window.height * 8 / 10)
+      .pointer_down(:left)
+      .pause(input, 0.2)
+      .move_to_location(window.width / 2, window.height / 10)
+      .pause(input, 0.2)
+      .release
+      .perform
+  end
 end
